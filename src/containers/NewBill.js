b91aed966574fc762897c33b4c5c50102597080c
NikX <41104633+NikXpro@users.noreply.github.com>
2024-12-10 14:20:08 +0100

    fix(NewBill): fix file type validation for uploaded images
    - Implemented validation to restrict file uploads to jpg, jpeg, and png formats.
    - Added user alert for unsupported file types to enhance user experience.

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -20,6 +20,16 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
+    
+    const fileExtension = fileName.split('.').pop().toLowerCase()
+    const allowedExtensions = ['jpg', 'jpeg', 'png']
+    
+    if (!allowedExtensions.includes(fileExtension)) {
+      e.target.value = ''
+      alert('Seuls les fichiers jpg, jpeg et png sont acceptés')
+      return
+    }
+
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
