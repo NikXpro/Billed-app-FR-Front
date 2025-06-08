4f8d252cd85268c71a51dfd89bc0c0b497ed1eff
NikX <41104633+NikXpro@users.noreply.github.com>
2025-06-08 20:52:38 +0200

    test(Dashboard): améliorer les tests pour la page Dashboard - réorganiser les importations pour une meilleure lisibilité - ajouter des tests pour vérifier le comportement lors de l'affichage des factures en attente, acceptées et refusées - s'assurer que les interactions avec les icônes de flèche et d'édition fonctionnent correctement - améliorer la gestion des erreurs lors de la récupération des factures depuis l'API

--- src/__tests__/Login.js
+++ src/__tests__/Login.js
@@ -2,10 +2,10 @@
  * @jest-environment jsdom
  */
 
-import LoginUI from "../views/LoginUI";
-import Login from "../containers/Login.js";
-import { ROUTES } from "../constants/routes";
 import { fireEvent, screen } from "@testing-library/dom";
+import { ROUTES } from "../constants/routes";
+import Login from "../containers/Login.js";
+import LoginUI from "../views/LoginUI";
 
 describe("Given that I am a user on login page", () => {
   describe("When I do not fill fields and I click on employee button Login In", () => {
